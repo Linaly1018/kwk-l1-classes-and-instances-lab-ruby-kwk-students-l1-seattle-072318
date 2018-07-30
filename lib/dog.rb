683e1adfefc8c7bb0dcb81ad82c6ac92c1d7d7cf
Linaly1018 <miyamoto.linaly@gmail.com>
2018-07-30 16:11:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,5 @@
 # dog.rb
 
 class Dog 
-  def initialize (dog)
-    @dog = dog
-  end
   
 end
-
-dog_one = Dog.new ("Fido")
-dog_two = Dog.new ("Snoopy")
-dog_three = Dog.new ("Lassie")
-
-dog_one
-dog_two
-dog_three
